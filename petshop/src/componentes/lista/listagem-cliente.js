83b8035e0edf01c716778d5ab1ac2c809c833e13
Luiz Silveira <l.filiphis@hotmail.com>
2020-08-04 21:26:03 -0300

    🔧  Configurando o webpack para buildar html, css e imagens

--- petshop/src/componentes/lista/listagem-cliente.js
+++ petshop/src/componentes/lista/listagem-cliente.js
@@ -1,8 +1,8 @@
-
+import { deletaCliente, listarClientes } from '../../api/cliente.js';
+import "../../assets/css/clientes.css";
 
 const removeCliente = (id) => {
-  if(confirm("Deseja deletar o cliente ?")){
-    debugger;
+  if (confirm("Deseja deletar o cliente ?")) {
     deletaCliente(id)
     window.location.reload()
   }
@@ -11,31 +11,24 @@ const removeCliente = (id) => {
 const corpoTabela = document.querySelector("[data-conteudo-tabela]");
 
 const exibeCliente = (cpf, nome, id) => {
-    const linha = document.createElement('tr');
+  const linha = document.createElement('tr');
 
-    const conteudoLinha = `
+  const conteudoLinha = `
     <td>${cpf}</td>
     <td>${nome}</td>
     <button type="button" class="btn btn-danger" onclick="removeCliente(${id})">Excluir</button>
     <a href="./edita/edita-clientes.html?id=${id}">
     <button type=""button class="btn btn-info">Editar</button>
     </a>
-    
-    
 `
-  
-    linha.innerHTML = conteudoLinha;
-    return linha;
-  };
-  
-  listarClientes().then( exibe => {
-  exibe.forEach(indice => {
-    corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome, indice.id))
-  })
- }
+  linha.innerHTML = conteudoLinha;
+  return linha;
+};
 
- )
-  
-  
- 
+listarClientes().then(exibe => {
+    exibe.forEach(indice => {
+      corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome, indice.id))
+    })
+  }
 
+)
